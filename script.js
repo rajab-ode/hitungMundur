20140c7fe286fcaa165327ba1d313b7aec9cb2ad
Rajab <67595457+rajab-ode@users.noreply.github.com>
2021-01-01 00:08:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const secondsEl = document.getElementById('seconds')
 
 // const newYears = '1 Jan 2022';
 
-function countdown(){
-  const newYearsDate= new Date(newYears);
-  const currentDate = new Date();
+// function countdown(){
+//   const newYearsDate= new Date(newYears);
+//   const currentDate = new Date();
 
-  const totalSeconds = (newYearsDate - currentDate) / 1000;
+//   const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60)% 60;
-  const second = Math.floor(totalSeconds) % 60;
+//   const days = Math.floor(totalSeconds / 3600 / 24);
+//   const hours = Math.floor(totalSeconds / 3600) % 24;
+//   const minutes = Math.floor(totalSeconds / 60)% 60;
+//   const second = Math.floor(totalSeconds) % 60;
   
-  daysEl.innerHTML = formatTime(days);
-  hoursEl.innerHTML = formatTime(hours);
+//   daysEl.innerHTML = formatTime(days);
+//   hoursEl.innerHTML = formatTime(hours);
   minsEl.innerHTML = formatTime(minutes);
   secondsEl.innerHTML = formatTime(second);
 
